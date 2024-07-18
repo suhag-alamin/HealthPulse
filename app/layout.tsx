c4fe1813f43b7063be494fb26b2218ac4e6c9098
Suhag Al Amin <developersuhag@gmail.com>
2024-07-18 12:42:43 +0600

    updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,11 +12,11 @@ const fontSans = FontSans({
 });
 
 export const metadata: Metadata = {
-  title: "CarePulse",
+  title: "HealthPulse",
   description:
     "A healthcare patient management System designed to streamline patient registration, appointment scheduling, and medical records management for healthcare providers.",
   icons: {
-    icon: "/assets/icons/logo-icon.svg",
+    icon: "/favicon.ico",
   },
 };
 
